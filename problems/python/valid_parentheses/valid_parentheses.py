c57b18a2b1d3ceacbdb8bb49e7996a0f7650ac77
Túlio Franco <tulioffranco199@gmail.com>
2025-03-07 09:33:52 -0300

    Add new version of valid parentheses

--- problems/python/valid_parentheses/valid_parentheses.py
+++ problems/python/valid_parentheses/valid_parentheses.py
@@ -1,58 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        pa = 0
-        co = 0
-        ch = 0
-        
-        if(len(s) % 2 != 0):
-            False
 
-        for valor in s:
-            if (valor == "{"):
-                ch += 1
-                stack.append(valor)
-            elif ( valor == "}"):
-                if ( not(ch > 0)):
-                    return False
-                else:
-                    if (stack[-1] != "{"):
-                        return False
-                    else:
-                        ch -= 1
-                        stack.pop()
+        for value in s:
+            if value in ('(','{','['):
+                stack.append(value)
+            elif value == ')' and len(stack) > 0 and stack[-1] == '(':
+                stack.pop()
+            elif value == '}' and len(stack) > 0 and stack[-1] == '{':
+                stack.pop()
+            elif value == ']' and len(stack) > 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                return False
 
-
-
-            elif (valor == "("):
-                pa += 1
-                stack.append(valor)
-
-            elif (valor == ")"):
-                if ( not(pa > 0)):
-                    return False
-                else:
-                    if (stack[-1] != "("):
-                        return False
-                    else:
-                        pa -= 1
-                        stack.pop()
-
-
-            elif (valor == "["):
-                co += 1
-                stack.append(valor)
-
-            elif (valor == "]"):
-                if ( not(co > 0)):
-                    return False
-                if (stack[-1] != "["):
-                    return False
-                else:
-                    co -= 1
-                    stack.pop()
-                    
-        if((co != 0) or (pa != 0) or (ch != 0)):
+        if len(stack) != 0:
             return False
-        else: 
-            return True
+        return True
